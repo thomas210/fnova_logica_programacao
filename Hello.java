9c5d1e9b2fbefdd9341f525134caa09fa40ea9ea
thomas210 <thomastabosa10@gmail.com>
2023-03-14 21:18:57 -0300

    update 2

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Nova Roma");
         System.out.println("Hello Git!");
+        System.out.println("2");
     }
 
 }
